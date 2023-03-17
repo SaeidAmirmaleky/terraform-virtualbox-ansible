115ec414dd8957655d5791eaaf6db5239657866e
Saeid Amirmaleky <amirmaleky.saeid@gmail.com>
2023-03-17 17:48:21 +0330

    Terraform main.tf file created and provider added

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    virtualbox = {
+      source  = "shekeriev/virtualbox"
+      version = "0.0.4"
+    }
+  }
+}
+
+provider "virtualbox" {
+  delay      = 60
+  mintimeout = 5
+}
+
