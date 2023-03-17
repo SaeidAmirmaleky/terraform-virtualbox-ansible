9e209959e0918e83748897b3b4dfb2468d040e91
Saeid Amirmaleky <amirmaleky.saeid@gmail.com>
2023-03-17 19:37:39 +0330

    add resource

--- main.tf
+++ main.tf
@@ -12,3 +12,25 @@ provider "virtualbox" {
   mintimeout = 5
 }
 
+resource "virtualbox_vm" "vm1" {
+  name = "ubuntu-vm1"
+  #Default_image_link
+  #image  = "https://app.vagrantup.com/shekeriev/boxes/debian-11/versions/0.2/providers/virtualbox.box"
+  #Ubuntu_image_link
+  #image  = "https://github.com/ccll/terraform-provider-virtualbox-images/releases"
+  image  = "../images/ubuntu-15.04.tar.xz"
+  cpus   = 1
+  memory = "512 mib"
+
+   network_adapter {
+     type           = "hostonly"
+     device         = "IntelPro1000MTDesktop"
+     host_interface = "vboxnet0"
+     # On Windows use this instead
+     # host_interface = "VirtualBox Host-Only Ethernet Adapter"
+   }
+}
+
+output "IPAddress" {
+   value = element(virtualbox_vm.vm1.*.network_adapter.0.ipv4_address, 1)
+}
