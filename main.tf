0d6af1f3953efa928650a3e7a34c1c200b160ad5
Saeid Amirmaleky <amirmaleky.saeid@gmail.com>
2023-03-18 19:17:35 +0330

    provider source changed

--- main.tf
+++ main.tf
@@ -1,36 +1,41 @@
 terraform {
   required_providers {
     virtualbox = {
-      source  = "shekeriev/virtualbox"
-      version = "0.0.4"
+      source = "terra-farm/virtualbox"
+      version = "0.2.2-alpha.1"
     }
   }
 }
 
 provider "virtualbox" {
-  delay      = 60
-  mintimeout = 5
+  # Configuration options
 }
 
-resource "virtualbox_vm" "vm1" {
-  name = "ubuntu-vm1"
-  #Default_image_link
-  #image  = "https://app.vagrantup.com/shekeriev/boxes/debian-11/versions/0.2/providers/virtualbox.box"
-  #Ubuntu_image_link
-  #image  = "https://github.com/ccll/terraform-provider-virtualbox-images/releases"
-  image  = "../images/ubuntu-15.04.tar.xz"
-  cpus   = 1
-  memory = "512 mib"
+# There are currently no configuration options for the provider itself.
 
-   network_adapter {
-     type           = "hostonly"
-     device         = "IntelPro1000MTDesktop"
-     host_interface = "vboxnet0"
-     # On Windows use this instead
-     # host_interface = "VirtualBox Host-Only Ethernet Adapter"
-   }
+resource "virtualbox_vm" "node" {
+  # count     = 2
+  # name      = format("node-%02d", count.index + 1)
+  # image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+  name      = "vm1"
+  image     = "../images/bionic-server-cloudimg-amd64-vagrant.box"
+  cpus      = 1
+  memory    = "512 mib"
+  #user_data = file("${path.module}/user_data")
+
+  network_adapter {
+    type           = "hostonly"
+    # On Linux, use this
+    # host_interface = "vboxnet1"
+    # On Windows, use this instead
+    host_interface = "VirtualBox Host-Only Ethernet Adapter"
+  }
 }
 
-output "IPAddress" {
-   value = element(virtualbox_vm.vm1.*.network_adapter.0.ipv4_address, 1)
+output "IPAddr" {
+  value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 1)
+}
+
+output "IPAddr_2" {
+  value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 2)
 }
